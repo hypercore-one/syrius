61fab73bc60d324a09d107fddb161b13fe62c01c
vilkris <vilkris@protonmail.com>
2023-05-17 21:23:43 +0300

    Disable P2P Swap UI when connected to a mainnet node

--- lib/widgets/tab_children_widgets/p2p_swap_tab_child.dart
+++ lib/widgets/tab_children_widgets/p2p_swap_tab_child.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:layout/layout.dart';
 import 'package:provider/provider.dart';
-import 'package:zenon_syrius_wallet_flutter/utils/notifiers/default_address_notifier.dart';
+import 'package:zenon_syrius_wallet_flutter/utils/utils.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/p2p_swap_widgets/p2p_swaps_card.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/p2p_swap_widgets/p2p_swap_options_card.dart';
-import 'package:zenon_syrius_wallet_flutter/widgets/reusable_widgets/layout_scaffold/standard_fluid_layout.dart';
+import 'package:zenon_syrius_wallet_flutter/widgets/widgets.dart';
 
 class P2pSwapTabChild extends StatelessWidget {
   final VoidCallback onStepperNotificationSeeMorePressed;
@@ -16,7 +16,11 @@ class P2pSwapTabChild extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _getLayout(context);
+    // TODO: Enable P2P Swaps UI on mainnet
+    return (kNodeChainId ?? 1) == 1
+        ? const SyriusErrorWidget(
+            'P2P Swaps can\'t be done on mainnet yet.\nPlease connect to a HyperCore One testnet node, such as: wss://testnet.deeznnutz.com:3598')
+        : _getLayout(context);
   }
 
   StandardFluidLayout _getLayout(BuildContext context) {
